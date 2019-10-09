5763813fae7047314e3c6d5a286d7e069ba4f4a9
vutivi <201141790@student.uj.ac.za>
2019-10-09 17:11:59 +0200

    fix: action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,8 @@ config.webpacker.check_yarn_integrity = false
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://earlybirds-co-za.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://earlybirds-co-za.herokuapp.com', /http:\/\/earlybirds-co-za.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
