39a51925ce51f718032a0e25352e2dcd63e382b9
vutivi <201141790@student.uj.ac.za>
2019-10-10 22:18:14 +0200

    chore: add slugger to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,19 @@
 class Category < ApplicationRecord
+    extend FriendlyId
+    friendly_id :slugger, use: :slugged
+
     validates :name, :description, presence: true
     validates_uniqueness_of :name
     validates :name, length: {minimum: 3, maximum: 30}
     validates :description, length: {minimum: 3, maximum: 500}
     has_many :events
+
+    def slugger
+        name
+    end
+    
+    def should_generate_new_friendly_id?
+        name_changed?
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_052404) do
+ActiveRecord::Schema.define(version: 2019_10_10_200824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,8 @@ ActiveRecord::Schema.define(version: 2019_10_10_052404) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_categories_on_slug", unique: true
   end
 
   create_table "events", force: :cascade do |t|
