de8fd42af7b8a78e032e3def1ce1f8df7e42e911
vutivi <201141790@student.uj.ac.za>
2019-10-16 16:20:18 +0200

    fix: css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  config.action_controller.asset_host = ENV.fetch(‘DIGITAL_OCEAN_SPACES_CDN_URL’)
-  config.action_mailer.asset_host = ENV.fetch(‘DIGITAL_OCEAN_SPACES_CDN_URL’)
+  config.action_controller.asset_host = ENV.fetch('DIGITAL_OCEAN_SPACES_CDN_URL')
+  config.action_mailer.asset_host = ENV.fetch('DIGITAL_OCEAN_SPACES_CDN_URL')
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
