06a8fff879d83f7998f1d98aef2414a587fb562f
vutivi <201141790@student.uj.ac.za>
2019-10-28 09:09:45 +0200

    chore: fix reset password

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,10 +1,10 @@
-# config/initializers/smtp.rb
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: '587',
-  authentication: :plain,
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  domain: 'earlybirds.co.za',
-  enable_starttls_auto: true
-}
+# # config/initializers/smtp.rb
+# ActionMailer::Base.smtp_settings = {
+#   address: 'smtp.sendgrid.net',
+#   port: '587',
+#   authentication: :plain,
+#   user_name: ENV['SENDGRID_USERNAME'],
+#   password: ENV['SENDGRID_PASSWORD'],
+#   domain: 'earlybirds.co.za',
+#   enable_starttls_auto: true
+# }
